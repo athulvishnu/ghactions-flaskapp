905839b9a2376c6a01691da99bb155f9bcfe0bb5
athul <atvishnu@proton.me>
2026-04-24 13:19:12 +0100

    updated app logic to include commit id

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    commit_id = os.getenv("COMMIT_SHA", "unknown")
+    return render_template("index.html", commit_id=commit_id)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
